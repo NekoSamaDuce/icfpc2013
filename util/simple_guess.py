d3ba06420f56855e54124fcd178c47f0ebb252ee
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2013-08-09 16:46:45 +0900

    Update simple_guess

--- util/simple_guess.py
+++ util/simple_guess.py
@@ -1,3 +1,4 @@
+import subprocess
 import sys
 import time
 
@@ -10,30 +11,48 @@ FLAGS = gflags.FLAGS
 gflags.DEFINE_string('id', None, 'Problem ID to solve')
 gflags.MarkFlagAsRequired('id')
 
-gflags.DEFINE_string('guess_file', None, 'Path to guess file')
-gflags.MarkFlagAsRequired('guess_file')
+gflags.DEFINE_string('solver', 'sandbox/kinaba/zentansaku', 'Path to solver')
+gflags.MarkFlagAsRequired('solver')
+
+gflags.DEFINE_string('problems_file', None, 'Path to problems file')
+gflags.MarkFlagAsRequired('problems_file')
 
 
 def main():
   sys.argv = FLAGS(sys.argv)
 
-  with open(FLAGS.guess_file) as f:
-    for program in f:
-      program = program.strip()
-      print '===', program
-      for trial in xrange(60):
-        try:
-          example = api.Guess(FLAGS.id, program)
-          print 'rejected. argument=0x%016x, expected=0x%016x, actual=0x%016x' % (
-              example.argument, example.expected, example.actual)
-          break
-        except api.RateLimited:
-          print '... rate limited ...'
-          if trial > 0:
-            time.sleep(1)
-        except api.Solved:
-          print '!!! SOLVED !!!'
-          sys.exit(0)
+  problems = {}
+  with open(FLAGS.problems_file) as f:
+    for line in f:
+      id, size, operators = line.rstrip('\n').split('\t')[:3]
+      size = int(size)
+      operators = operators.split(',')
+      problem = api.Problem(id, size, operators)
+      problems[problem.id] = problem
+
+  assert FLAGS.id in problems, 'Unknown --id specified'
+  problem = problems[FLAGS.id]
+
+  solver_output = subprocess.check_output(
+      [FLAGS.solver, '--size=%d' % problem.size,
+       '--operators=%s' % ','.join(problem.operators)])
+  programs = solver_output.splitlines()
+
+  for program in programs:
+    print '===', program
+    for trial in xrange(60):
+      try:
+        example = api.Guess(FLAGS.id, program)
+        print 'rejected. argument=0x%016x, expected=0x%016x, actual=0x%016x' % (
+            example.argument, example.expected, example.actual)
+        break
+      except api.RateLimited:
+        print '... rate limited ...'
+        if trial > 0:
+          time.sleep(1)
+      except api.Solved:
+        print '!!! SOLVED !!!'
+        sys.exit(0)
   print 'NO PROGRAM WAS ACCEPTED. DEAD END...'
 
 
