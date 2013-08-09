0a66c75a02e6bbed5dc815de3e96adfbda7f6097
phoenixstarhiro <dummy@email>
2013-08-09 03:31:10 -0700

    Port Clustering algorithm from kinaba-san's code.

---
+++ solver/cluster.h
@@ -0,0 +1,43 @@
+#ifndef ICFPC_CLUSTER_H_
+#define ICFPC_CLUSTER_H_
+
+#include <map>
+#include <vector>
+#include <memory>
+#include <random>
+
+#include "expr.h"
+
+namespace icfpc {
+
+// Creates a key (vector of 256 uint64 values).
+std::vector<uint64_t> CreateKey() {
+  static std::mt19937 rand_engine(178);
+  std::uniform_int_distribution<uint64_t> rand_distr(0, ~0ull);
+  std::vector<uint64_t> keys;
+  keys.reserve(256);
+  for (size_t i = 0; i < 256; ++i) {
+    keys.emplace_back(rand_distr(rand_engine));
+  }
+  return keys;
+}
+
+// Classify by key.
+std::map<std::vector<uint64_t>, std::vector<std::shared_ptr<Expr> > >
+CreateCluster(const std::vector<uint64_t>& input,
+              const std::vector<std::shared_ptr<Expr> >& expr_list) {
+  std::map<std::vector<uint64_t>, std::vector<std::shared_ptr<Expr> > > result;
+  for (std::shared_ptr<Expr> expr : expr_list) {
+    std::vector<uint64_t> key;
+    key.reserve(256);
+    for (uint64_t v : input) {
+      key.push_back(Eval(*expr, v));
+    }
+    result[key].push_back(expr);
+  }
+  return result;
+}
+
+}  // namespace icfpc
+
+#endif  // ICFPC_CLUSTER_H_
--- solver/util.h
+++ solver/util.h
@@ -6,4 +6,16 @@
 
 #define NOTREACHED() {assert(false); abort();}
 
+template<typename T>
+void PrintCollection(std::ostream* os, const T& collection, const std::string& joiner) {
+  if (collection.empty()) return;
+
+  auto iter = collection.cbegin();
+  *os << *iter;
+  auto end = collection.cend();
+  while (++iter != end) {
+    *os << joiner << *iter;
+  }
+}
+
 #endif  // ICFPC_UTIL_H_
