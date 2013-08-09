ce26e88890464457275ef248d9f93326d057ed14
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2013-08-10 01:27:40 +0900

    Introduce --mode=oneoff

--- frontend/frontend_util.py
+++ frontend/frontend_util.py
@@ -7,14 +7,14 @@ from util import api
 FLAGS = gflags.FLAGS
 
 gflags.DEFINE_enum(
-    'mode', None, ['train', 'serious'],
+    'mode', None, ['train', 'oneoff', 'serious'],
     'Specifies how the frontend reads a problem. '
     '"train" generates a training problem automatically beforehand with '
     '--size and --operators. "serious" picks up a problem from a TSV file '
     'specified by --problemset_file with ID specified by --id.')
 gflags.MarkFlagAsRequired('mode')
 
-# Flags for --mode=train
+# Flags for --mode=train / --mode=oneoff
 gflags.DEFINE_integer(
     'size', None,
     'The training problem size to generate.')
@@ -26,6 +26,8 @@ gflags.DEFINE_string(
 gflags.DEFINE_string(
     'problemset_file', None,
     'Path to the problemset TSV file.')
+
+# Flags for --mode=oneoff / --mode=oneoff
 gflags.DEFINE_string(
     'problem_id', None,
     'The problem ID to pick from the problem set specified by '
@@ -36,6 +38,11 @@ def GetProblemByFlags():
   if FLAGS.mode == 'train':
     logging.info('Requesting a training problem...')
     problem = api.Train(FLAGS.size, FLAGS.operators)
+  elif FLAGS.mode == 'oneoff':
+    assert FLAGS.problem_id is not None
+    assert FLAGS.size is not None
+    assert FLAGS.operators is not None
+    problem = api.Problem(FLAGS.problem_id, FLAGS.size, FLAGS.operators.split(','))
   else:  # FLAGS.mode == 'serious'
     assert FLAGS.problem_id, '--problem_id must be specified'
     with open(FLAGS.problemset_file) as f:
