97f9daa6c1ab0e5c7cf98ff4b4b93954a9f66174
kinaba <kiki@kmonos.net>
2013-08-10 15:58:47 +0900

    Add genall_unittest.

--- solver/genall.cc
+++ solver/genall.cc
@@ -3,13 +3,11 @@
 
 #include "expr.h"
 #include "expr_list.h"
-#include "expr_list_old.h"
 
 using namespace icfpc;
 
 DEFINE_int32(size, -1, "Size of the expression");
 DEFINE_string(operators, "", "List of the operators");
-DEFINE_bool(genallcheck, false, "Check by comparison with the old routine");
 
 int main(int argc, char* argv[]) {
   google::InstallFailureSignalHandler();
@@ -23,14 +21,6 @@ int main(int argc, char* argv[]) {
   int op_type_set = ParseOpTypeSet(FLAGS_operators);
 
   std::vector<std::shared_ptr<Expr> > result = ListExpr(FLAGS_size, op_type_set);
-  if (FLAGS_genallcheck) {
-    std::vector<std::shared_ptr<Expr> > result2 = old::ListExpr(FLAGS_size, op_type_set);
-    CHECK_EQ(result.size(), result2.size());
-    for (size_t i = 0; i < result2.size(); ++i)
-      CHECK(result[i]->EqualTo(*result2[i]));
-    std::cerr<<"CHECKED"<<std::endl;
-  }
-
   for (const std::shared_ptr<Expr>& e : result) {
     std::cout << *e << std::endl;
   }
---
+++ solver/genall_unittest.cc
@@ -0,0 +1,55 @@
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+
+#include "expr.h"
+#include "expr_list.h"
+#include "expr_list_old.h"
+
+using namespace icfpc;
+
+TEST(GenAllTest, A) {
+  int size = 3;
+  int op_type_set = ParseOpTypeSet("not");
+  std::vector<std::shared_ptr<Expr> > result = ListExpr(size, op_type_set);
+  std::vector<std::shared_ptr<Expr> > result_old = old::ListExpr(size, op_type_set);
+  EXPECT_EQ(result_old.size(), result.size());
+  for (size_t i = 0; i < result.size(); ++i)
+    EXPECT_TRUE(result[i]->EqualTo(*result_old[i]));
+}
+
+TEST(GenAllTest, B) {
+  int size = 10;
+  int op_type_set = ParseOpTypeSet("not,if0,fold,plus");
+  std::vector<std::shared_ptr<Expr> > result = ListExpr(size, op_type_set);
+  std::vector<std::shared_ptr<Expr> > result_old = old::ListExpr(size, op_type_set);
+  EXPECT_EQ(result_old.size(), result.size());
+  for (size_t i = 0; i < result.size(); ++i)
+    EXPECT_TRUE(result[i]->EqualTo(*result_old[i]));
+}
+
+TEST(GenAllTest, C) {
+  int size = 10;
+  int op_type_set = ParseOpTypeSet("shr1,if0,shl1,or");
+  std::vector<std::shared_ptr<Expr> > result = ListExpr(size, op_type_set);
+  std::vector<std::shared_ptr<Expr> > result_old = old::ListExpr(size, op_type_set);
+  EXPECT_EQ(result_old.size(), result.size());
+  for (size_t i = 0; i < result.size(); ++i)
+    EXPECT_TRUE(result[i]->EqualTo(*result_old[i]));
+}
+
+TEST(GenAllTest, D) {
+  int size = 10;
+  int op_type_set = ParseOpTypeSet("shl1,if0,tfold");
+  std::vector<std::shared_ptr<Expr> > result = ListExpr(size, op_type_set);
+  std::vector<std::shared_ptr<Expr> > result_old = old::ListExpr(size, op_type_set);
+  EXPECT_EQ(result_old.size(), result.size());
+  for (size_t i = 0; i < result.size(); ++i)
+    EXPECT_TRUE(result[i]->EqualTo(*result_old[i]));
+}
+
+int main(int argc, char **argv) {
+  google::InstallFailureSignalHandler();
+  google::InitGoogleLogging(argv[0]);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
