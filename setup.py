694457bb7c7755576f2b7f28a1108ca52d98c989
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2013-08-10 16:03:09 +0900

    Revert yui.

--- setup.py
+++ setup.py
@@ -3,18 +3,10 @@ import time
 
 from distutils.core import setup, Extension
 
-module1 = Extension(
-    'solver.yui',
-    sources=['solver/yui.cc'],
-    depends=glob.glob('solver/*.h'),
-    extra_compile_args=['-std=c++0x'],
-    libraries=['glog'])
-
 setup(
     name='icfpc2013',
     version=str(int(time.time())),
-    packages=['sandbox'],
-    ext_modules=[module1],
+    packages=['frontend', 'sandbox', 'solver'],
     install_requires=[
         'python-gflags',
         'requests',
